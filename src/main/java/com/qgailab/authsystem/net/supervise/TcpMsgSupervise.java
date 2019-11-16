bac1c6a83cc8657fb2b087ba7ef12023f28bd8f8
SheldonPeng <775887734@qq.com>
2019-11-16 22:37:30 +0800

    新增Tcp信息的暂存区，完成Tcp客户端消息控制器与消息控制器

---
+++ src/main/java/com/qgailab/authsystem/net/supervise/TcpMsgSupervise.java
@@ -0,0 +1,86 @@
+package com.qgailab.authsystem.net.supervise;
+
+import com.qgailab.authsystem.constance.MachineType;
+import io.netty.channel.Channel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * @Description : TCP消息控制器
+ * @Param :
+ * @Return :
+ * @Author : SheldonPeng
+ * @Date : 2019-11-16
+ */
+
+public class TcpMsgSupervise {
+
+    /**
+     * @Description : 检查机器健康状态
+     * @Param : [machineId, machineType]
+     * @Return : boolean
+     * @Author : SheldonPeng
+     * @Date : 2019-11-16
+     */
+    public static boolean checkMachineHealth(Integer machineId, MachineType machineType){
+
+        Channel channel = ChannelSupervise.findChannel(machineId, machineType);
+        if ( null == channel){
+            return false;
+        }
+        channel.writeAndFlush("ready");
+        return true;
+    }
+
+    /**
+     * @Description : 读取身份证信息
+     * @Param : [machineId]
+     * @Return : boolean
+     * @Author : SheldonPeng
+     * @Date : 2019-11-16
+     */
+    public static boolean loadIdInfomation(Integer machineId){
+
+        Channel channel = ChannelSupervise.findChannel(machineId,MachineType.IdCardMachine);
+        if ( null == channel ){
+            return false;
+        }
+        channel.writeAndFlush("load");
+        return true;
+    }
+
+
+    /**
+     * @Description : 读取指纹信息
+     * @Param : [machineId]
+     * @Return : boolean
+     * @Author : SheldonPeng
+     * @Date : 2019-11-16
+     */
+    public static boolean loadFingerInfomation(Integer machineId){
+
+        Channel channel = ChannelSupervise.findChannel(machineId,MachineType.FingerMachine);
+        if ( null == channel){
+            return false;
+        }
+        channel.writeAndFlush("load");
+        return true;
+    }
+
+    /**
+     * @Description : 读取签名信息
+     * @Param : [machineId]
+     * @Return : boolean
+     * @Author : SheldonPeng
+     * @Date : 2019-11-16
+     */
+    public static boolean loadSignatureInfomation(Integer machineId) {
+
+        Channel channel = ChannelSupervise.findChannel(machineId, MachineType.SignatureMachine);
+        if (null == channel) {
+            return false;
+        }
+        channel.writeAndFlush("load");
+        return true;
+    }
+}
